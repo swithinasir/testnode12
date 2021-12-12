248fdedc171c934726280a55ffe9aacd997e9498
swithinasir <68471869+swithinasir@users.noreply.github.com>
2021-12-12 21:43:01 +0530

    testnode12

--- index.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-const app=express()
-app.get('/',function(req,res){
-    res.redirect('/log')
-})
-app.get('/log',function(req,res){
-    res.send("hi")
-})
-app.listen(8080)
